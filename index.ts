2e890889c39939018fcece1f35bdf132f21d743a
Kacper Mazur <kacper5103@gmail.com>
2023-10-15 00:49:37 +0100

    working on spot testnet

--- index.ts
+++ index.ts
@@ -1,76 +1,101 @@
 import dotenv from 'dotenv';
-dotenv.config();
-
-import express from 'express';
-import { Server } from 'socket.io';
-import http from 'http';
-import Binance from 'node-binance-api';
+dotenv.config(); 
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 
-const app = express();
-const PORT = 4000;
+const { Spot } = require('@binance/connector');
+
+const app: express.Application = express();
+app.use(cors()); // Allows all origins by default. Adjust for production!
+
+const port: number = 5000;
+
+const apiKey: string = process.env.BINANCE_API_KEY || '';
+const apiSecret: string = process.env.BINANCE_SECRET_KEY || '';
+
+const client = new Spot(apiKey, apiSecret, { baseURL: 'https://testnet.binance.vision'});
 
 interface BinanceError {
-  body: string;
-  code?: number;
-  message?: string;
+    response?: {
+        data: any;
+    };
 }
 
-interface BinanceResponse {
-  symbol: string;
-  orderId: number;
-  // ... other properties of the Binance order response
-}
-
-// Initialize Binance with futures testnet options
-const binance = new Binance().options({
-  APIKEY: process.env.BINANCE_API_KEY!,
-  APISECRET: process.env.BINANCE_SECRET_KEY!,
-  useServerTime: true,
-  test: true,
-  baseURL: 'https://testnet.binancefuture.com', // Point to futures testnet
-  futures: true  // This enables futures trading on the Binance API
-});
-
-app.use(cors());
-
-// Create an HTTP server instance and pass the Express app to it
-const httpServer = http.createServer(app);
-
-// Setup Socket.io with the HTTP server and CORS options
-const io = new Server(httpServer, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
-});
-
-io.on('connection', (socket) => {
-  console.log('User connected:', socket.id);
-
-  // Listen for order requests from frontend
-  socket.on('placeOrder', async (data) => {
+app.get('/balance', async (req: Request, res: Response) => {
     try {
-        console.info( await binance.futuresBalance() );
-
-      // Place a futures order
-      binance.futuresBuy(data.symbol, data.quantity, data.price, { type: 'LIMIT' }, (error: BinanceError, response: BinanceResponse) => {
-        if (error) {
-          socket.emit('orderError', JSON.parse(error.body).msg);
-        } else {
-          socket.emit('orderResult', response);
-        }
-      });
+        const response = await client.account();
+        res.json(response.data.balances);
     } catch (error) {
-      if (error instanceof Error) {
-        socket.emit('orderError', error.message);
-      } else {
-        socket.emit('orderError', 'An unknown error occurred.');
-      }
+        if (error instanceof Error) { // Check if error is an instance of the Error class
+            res.status(500).json({ error: error.message });
+        } else {
+            res.status(500).json({ error: 'An unknown error occurred' });
+        }
     }
-  });
 });
 
-httpServer.listen(PORT, () => {
-  console.log(`Server started on http://localhost:${PORT}`);
+app.post('/buy-bitcoin', async (req: Request, res: Response) => {
+    try {
+        const response = await client.newOrder('BTCUSDT', 'BUY', 'MARKET', { quantity: 0.5 });
+        console.log('Binance Response:', response); 
+        res.json(response.data);
+    } catch (error) {
+        const binanceError = error as BinanceError;
+
+        if (binanceError.response?.data) {
+            // Log the Binance Data Error to the console
+            console.error('Binance Data Error:', binanceError.response.data); 
+
+            // Check the msg from Binance and handle specific errors
+            if (binanceError.response.data.code === -2010) {
+                res.status(400).json({ error: 'You have insufficient funds to complete this action.' });
+            } else {
+                // Handle other Binance specific errors or messages here if needed, 
+                // for now, we'll send the Binance msg directly to the user
+                res.status(500).json({ error: binanceError.response.data.msg });
+            }
+        } else if (error instanceof Error) {
+            res.status(500).json({ error: error.message });
+        } else {
+            res.status(500).json({ error: 'An unknown error occurred' });
+        }
+    }
+});
+
+
+
+app.post('/buy-ethereum', async (req: Request, res: Response) => {
+    try {
+        const response = await client.newOrder('ETHUSDT', 'BUY', 'MARKET', { quantity: 0.5 });
+        res.json(response.data);
+    } catch (error) {
+        if (error instanceof Error) { // Check if error is an instance of the Error class
+            res.status(500).json({ error: error.message });
+        } else {
+            res.status(500).json({ error: 'An unknown error occurred' });
+        }
+    }
+});
+
+app.post('/sell/:coin', async (req: Request, res: Response) => {
+    const coin = req.params.coin.toUpperCase();
+    
+    if (!['BTC', 'ETH'].includes(coin)) {
+        return res.status(400).json({ error: 'Unsupported coin' });
+    }
+    
+    try {
+        const response = await client.newOrder(`${coin}USDT`, 'SELL', 'MARKET', { quantity: 0.5 });
+        res.json(response.data);
+    } catch (error) {
+        if (error instanceof Error) { // Check if error is an instance of the Error class
+            res.status(500).json({ error: error.message });
+        } else {
+            res.status(500).json({ error: 'An unknown error occurred' });
+        }
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
 });
